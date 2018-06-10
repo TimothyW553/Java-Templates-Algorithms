2eb8d8bf115c979311343afe0deb499691dc4f9d
TimothyW553 <31230953+TimothyW553@users.noreply.github.com>
2018-06-09 23:07:18 -0400

    Update Dijkstra.java

--- Graph Theory/shortestpath/Dijkstra.java
+++ Graph Theory/shortestpath/Dijkstra.java
@@ -1,41 +1,21 @@
-/*
- * Dijkstra's algorithm solves the single-source shortest path (SSSP) problem. It does not work with negative edge weights.
- *
- * Time complexity: O(V^2)
- */
-
-package codebook.graph.shortestpath;
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.io.PrintWriter;
-import java.util.ArrayList;
-import java.util.StringTokenizer;
+import java.util.*;
+import java.io.*;
 
 public class Dijkstra {
-
-  static BufferedReader br;
-  static PrintWriter out;
-  static StringTokenizer st;
-
+  
   static int n, m, orig, dest;
   static ArrayList<ArrayList<Edge>> adj;
   static int[] dist;
   static boolean[] v;
 
-  public static void main (String[] args) throws IOException {
-    br = new BufferedReader(new InputStreamReader(System.in));
-    out = new PrintWriter(new OutputStreamWriter(System.out));
-    //br = new BufferedReader(new FileReader("in.txt"));
-    //out = new PrintWriter(new FileWriter("out.txt"));
+  public static void main (String[] args) {
+    Scanner sc = new Scanner(System.in);
 
-    n = readInt();
-    m = readInt();
+    n = sc.nextInt();
+    m = sc.nextInt();
 
-    orig = readInt() - 1;
-    dest = readInt() - 1;
+    orig = sc.nextInt() - 1;
+    dest = sc.nextInt() - 1;
 
     adj = new ArrayList<ArrayList<Edge>>();
 
@@ -48,9 +28,9 @@ public class Dijkstra {
     }
 
     for (int i = 0; i < m; i++) {
-      int a = readInt() - 1;
-      int b = readInt() - 1;
-      int c = readInt();
+      int a = sc.nextInt() - 1;
+      int b = sc.nextInt() - 1;
+      int c = sc.nextInt();
       adj.get(a).add(new Edge(b, c));
     }
     dist[orig] = 0;
@@ -65,8 +45,8 @@ public class Dijkstra {
         dist[e.dest] = Math.min(dist[e.dest], dist[minIndex] + e.cost);
     }
 
-    out.println(dist[dest]);
-    out.close();
+    System.out.println(dist[dest]);
+    System.out.close();
   }
 
   static class Edge {
@@ -76,31 +56,5 @@ public class Dijkstra {
       this.dest = dest;
       this.cost = cost;
     }
-  }
-
-  static String next () throws IOException {
-    while (st == null || !st.hasMoreTokens())
-      st = new StringTokenizer(br.readLine().trim());
-    return st.nextToken();
-  }
-
-  static long readLong () throws IOException {
-    return Long.parseLong(next());
-  }
-
-  static int readInt () throws IOException {
-    return Integer.parseInt(next());
-  }
-
-  static double readDouble () throws IOException {
-    return Double.parseDouble(next());
-  }
-
-  static char readCharacter () throws IOException {
-    return next().charAt(0);
-  }
-
-  static String readLine () throws IOException {
-    return br.readLine().trim();
   }
 }
